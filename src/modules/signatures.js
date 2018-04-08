f9acad99cf28008fe257db2442ff3167ab69d202
XIONG ZHEN <alex.xiong@entu.io>
2018-04-08 11:51:29 +0800

    增加必须要用到的检测req参数合规性的request-sanitizer类，以及初始化delegates, multisignatures和signatures模块代码

---
+++ src/modules/signatures.js
@@ -0,0 +1,92 @@
+var util = require('util');
+var extend = require('extend');
+var async = require('async');
+var path = require('path');
+var fs = require('fs');
+var sandboxHelper = require('../utils/sandbox.js');
+
+// private objects
+var modules_loaded, library, self, privated = {}, shared = {};
+
+function Signature() {
+
+    this.calculateFee = function (txObj, sender) {
+
+    };
+
+    this.create = function (data, txObj) {
+
+    };
+
+    this.objectNormalize = function (txObj) {
+
+    };
+
+    this.getBytes = function (txObj) {
+
+    };
+
+    this.ready = function (txObj, sender) {
+
+    };
+
+    this.process = function (txObj, sender, cb) {
+
+    };
+
+    this.verify = function (txObj, sender, cb) {
+
+    };
+
+    this.apply = function (txObj, blockObj, sender, cb) {
+
+    };
+
+    this.undo = function (txObj, blockObj, sender, cb) {
+
+    };
+
+    this.applyUnconfirmed = function (txObj, sender, cb) {
+
+    };
+
+    this.undoUnconfirmed = function (txObj, sender, cb) {
+
+    };
+
+    this.load = function (raw) {
+
+    };
+
+    this.save = function (txObj, cb) {
+
+    };
+}
+
+// constructor
+function Signatures(cb, scope) {
+    library = scope;
+    self = this;
+    self.__private = privated;
+
+    setImmediate(cb, null, self);
+}
+
+// public methods
+Signatures.prototype.sandboxApi = function (call, args, cb) {
+    sandboxHelper.callMethod(shared, call, args, cb);
+};
+
+Signatures.prototype.callApi = function (call, args, cb) {
+    var callArgs = [args, cb];
+    // execute
+    shared[call].apply(null, callArgs);
+};
+
+// Events
+Signatures.prototype.onInit = function (scope) {
+    modules_loaded = scope && scope != undefined ? true : false;
+};
+
+// export
+module.exports = Signatures;
