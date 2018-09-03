f0ae590fd2010389c9ec9a4308be4af5bb96b96d
shang <297157489@qq.com>
2018-09-03 15:31:07 +0800

    build

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -2,6 +2,6 @@ const baseUrl = '//10.0.248.211'
 // const baseUrl = '//0.0.0.0'
 
 export default {
-    url: `http:${baseUrl}:8000/`,
+    url: `http:${baseUrl}:7259/`,
     websocketUrl: `ws:${baseUrl}:18008`
 }
