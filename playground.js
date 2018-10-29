55381c2a5cec4de823e49b2a9557294bcd6f7681
陈新游 <draven.chen@solarsoft.org>
2018-10-29 17:47:15 +0800

    修改配置文件的端口和传世区块的交易信息

--- playground.js
+++ playground.js
@@ -4,8 +4,9 @@
 // 172.31.6.143
 // 172.31.3.81
 let ip = require("ip");
-console.log(ip.toLong("172.31.13.173"));
-console.log(ip.toLong("172.31.15.91"));
-console.log(ip.toLong("172.31.15.56"));
-console.log(ip.toLong("172.31.6.143"));
-console.log(ip.toLong("172.31.3.81"));
+console.log(ip.toLong("13.251.35.155"));
+console.log(ip.toLong("3.0.49.147"));
+console.log(ip.toLong("13.251.63.111"));
+console.log(ip.toLong("13.251.129.96"));
+console.log(ip.toLong("13.229.147.236"));
+console.log(ip.toLong("13.229.137.253"));
