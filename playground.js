30c622fca87d16322ca5ef4d76fac5ab36bc8452
陈新游 <draven.chen@solarsoft.org>
2018-10-30 10:55:51 +0800

    修改配置文件的端口和创世区块的交易信息

--- playground.js
+++ playground.js
@@ -1,12 +1,14 @@
-// 172.31.13.173
-// 172.31.15.91
-// 172.31.15.56
-// 172.31.6.143
-// 172.31.3.81
+// bac-testnet-1    172.31.13.173    13.251.35.155
+// bac-testnet-2    172.31.15.91    3.0.49.147
+// bac-testnet-3    172.31.15.56    13.229.142.176
+// bac-testnet-4    172.31.6.143    13.251.130.204
+// bac-testnet-5    172.31.3.81    13.229.147.236
+// bac-testnet-6    172.31.10.46    13.229.137.253
+
+
 let ip = require("ip");
-console.log(ip.toLong("13.251.35.155"));
-console.log(ip.toLong("3.0.49.147"));
-console.log(ip.toLong("13.229.142.176"));
-console.log(ip.toLong("13.251.130.204"));
-console.log(ip.toLong("13.229.147.236"));
-console.log(ip.toLong("13.229.137.253"));
+console.log(ip.toLong("172.31.13.173"));
+console.log(ip.toLong("172.31.15.91"));
+console.log(ip.toLong("172.31.15.56"));
+console.log(ip.toLong("172.31.6.143"));
+console.log(ip.toLong("172.31.3.81"));
