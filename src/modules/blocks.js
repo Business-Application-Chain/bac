a32c536ce0f9d17f1435525d9431564c460f6d76
陈新游 <draven.chen@solarsoft.org>
2018-11-28 10:55:56 +0800

    修复同步

--- src/modules/blocks.js
+++ src/modules/blocks.js
@@ -17,7 +17,7 @@ var Json2csv = require('json2csv').Parser;
 
 var header = ['b_hash', 'b_version', 'b_timestamp', 'b_height', 'b_previousBlock', 'b_numberOfTransactions', 'b_totalAmount', 'b_totalFee','b_reward','b_generatorPublicKey','b_blockSignature', 'b_merkleRoot', 'b_difficulty', 'b_basic', 'b_decisionSignature', 'b_decisionAddress', 'b_minerHash',
     't_hash', 't_type','t_timestamp','t_senderPublicKey', 't_senderId','t_recipientId','t_senderUsername','t_recipientUsername','t_amount','t_fee','t_signature','t_signSignature', 's_publicKey', 'd_address',
-    'da_hash', 'da_issuersAddress','da_className', 'da_abi', 'da_tokenList', 'do_dappHash', 'do_fun', 'do_params', 'i_name', 'i_desc','i_issuersAddress', 'dt_dappHash', 'dt_dapp',
+    'da_hash', 'da_issuersAddress','da_className', 'da_abi', 'da_tokenList', 'do_dappHash', 'do_fun', 'do_params', 'i_name', 'i_desc','i_issuersAddress', 'dt_dappHash',
     'c_address','u_alias', 'm_min','m_lifetime','m_keysgroup','t_requesterPublicKey','t_signatures', 'a_name', 'a_description', 'a_hash', 'a_decimal', 'a_total', 'tr_amount', 'tr_assetsHash', 'tr_assetsName', 'l_lockHeight', 'min_ip', 'min_port'];
 
 require('array.prototype.findindex'); // Old node fix
@@ -76,7 +76,6 @@ privated.blocksDataFields = {
     'i_desc': String,
     'i_issuersAddress': String,
     'dt_dappHash': String,
-    'dt_dapp': String,
 
     'c_address': String,
     'u_alias': String,
@@ -98,17 +97,16 @@ privated.blocksDataFields = {
     'min_port': Number,
 
 };
-
 privated.serchSql = 'SELECT '+
     'b.hash as b_hash, b.version as b_version, b.timestamp as b_timestamp, b.height as b_height, b.previousBlock as b_previousBlock, b.numberOfTransactions as b_numberOfTransactions, b.totalAmount as b_totalAmount, b.totalFee as b_totalFee, b.reward as b_reward,  b.generatorPublicKey as b_generatorPublicKey, b.blockSignature as b_blockSignature,' +
     'b.merkleRoot as b_merkleRoot, b.difficulty as b_difficulty, b.basic as b_basic, b.decisionSignature as b_decisionSignature, b.decisionAddress as b_decisionAddress, b.minerHash as b_minerHash, ' +
     't.hash as t_hash, t.type as t_type, t.timestamp as t_timestamp, t.senderPublicKey as t_senderPublicKey, t.senderId as t_senderId, t.recipientId as t_recipientId, t.senderUsername as t_senderUsername, t.recipientUsername as t_recipientUsername, t.amount as t_amount, t.fee as t_fee, t.signature as t_signature, t.signSignature as t_signSignature,  ' +
     's.publicKey as s_publicKey, ' +
     'd.address as d_address, ' +
-    'da.hash as da_hash, da.className as da_className, da.issuersAddress as da_issuersAddress, da.abi as da_abi, da.tokenList as da_tokenList, ' +
+    'da.hash as da_hash, da.issuersAddress as da_issuersAddress, da.className as da_className, da.abi as da_abi, da.tokenList as da_tokenList, ' +
     'do.dappHash as do_dappHash, do.fun as do_fun, do.params as do_params, '+
-    'dt.dappHash as dt_dappHash, ' +
     'i.name as i_name, i.desc as i_desc, i.issuersAddress as i_issuersAddress, '+
+    'dt.dappHash as dt_dappHash, ' +
     'c.address as c_address, ' +
     'u.username as u_alias,' +
     'm.min as m_min, m.lifetime as m_lifetime, m.keysgroup as m_keysgroup, ' +
