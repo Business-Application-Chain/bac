03ca47406bac9b4d8aa2f03885c82151ab5db0d1
heyijun <heyijun1118@163.com>
2019-01-17 17:00:44 +0800

    修改字段名 b_height

--- public/src/views/explorer/List.vue
+++ public/src/views/explorer/List.vue
@@ -168,7 +168,7 @@
 
             },
             loadBlocksMore ($state) {
-                api.blocks.blocks([this.blocksList[this.blocksList.length - 1].height, 50]).then(res => {
+                api.blocks.blocks([this.blocksList[this.blocksList.length - 1].b_height, 50]).then(res => {
                     if (res === null) return;
                     if (res.length == 50) {
                         $state.loaded()
